449499fe830db606f6ef50a9821572f4368e7297
David Zuluaga <zuluroa@gmail.com>
2021-11-24 19:11:57 -0500

    Se agrega la funcionalidad para traer las listas creadas y mostrarlas en pantalla

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { createContext, useReducer, useContext, useRef, useState } from 'react';
+import React, { createContext, useReducer, useContext, useEffect, useRef, useState } from 'react';
 
 const HOST_API = "http://localhost:8080/api"
 const initialState = {
@@ -39,21 +39,56 @@ const Form = () => {
   }
 
 
-
   return <form ref={formRef}>
     <input type="text" name="name" onChange={(event) => {
       setState({ ...state, name: event.target.value })
     }}></input>
-    {!item.id && <button onClick={onAdd}>Agregar</button>}
+    <button onClick={onAdd}>Agregar</button>
   </form>
 
 }
 
 
+const List = () => {
 
+  const { dispatch, state } = useContext(Store);
+
+  useEffect(() => {
+    fetch(HOST_API + "/todos")
+      .then(response => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list })
+      })
+  }, [state.list.length, dispatch]);
+
+
+  return <div>
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Nombre</td>
+          <td>¿Esta completado?</td>
+        </tr>
+      </thead>
+      <tbody>
+        {state.list.map((todo) => {
+          return <tr key={todo.id}>
+            <td>{todo.id}</td>
+            <td>{todo.name}</td>
+            <td>{todo.isCompleted === true ? "SI" : "NO"}</td>
+          </tr>
+        })}
+      </tbody>
+
+    </table>
+  </div>
+}
 
 function reducer(state, action) {
   switch (action.type) {
+    case 'update-list':
+      return { ...state, list: action.list }
     case 'add-item':
       const newList = state.list;
       newList.push(action.item);
@@ -76,6 +111,7 @@ const StoreProvider = ({ children }) => {
 function App() {
   return <StoreProvider>
     <Form />
+    <List />
   </StoreProvider>
 }
 
